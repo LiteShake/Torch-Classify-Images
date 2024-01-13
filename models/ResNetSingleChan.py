71cefe26703db8b2be5b821b4fbe304cf37d16a3
LiteShake <strawberryshakelite@gmail.com>
2024-01-13 09:38:09 +0530

    Added resnet files but they are actually vanilla convnet

---
+++ models/ResNetSingleChan.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ResNetBlock(nn.Module):
+    
+    def __init__(self, filters):
+        super.__init__
+        
+    
+class ConvSingleChan(nn.Module):
+    
+    def __init__(self) :
+        super().__init__()
+        self.conv2 = nn.Conv2d(1, 4, 5, 1, 2)
+        self.pool = nn.AvgPool2d(2, 2)
+        self.conv2 = nn.Conv2d(4, 16, 5, 1, 2)
+        self.conv3 = nn.Conv2d(16, 32, 5, 1, 2)
+        self.fc1 = nn.Linear(1000, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear( 64, 10)
+        
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 0)
+        # print(x.shape)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.sigmoid(self.fc3(x))
+        
+        return x
---
+++ models/ResNetThreeChan.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ConvThreeChan(nn.Module):
+    
+    def __init__(self) :
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(16 * 5 * 5, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear( 64, 10)
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        
+        return x
